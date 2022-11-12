b061d70cf2f339ece5284df98f46f06c6221b645
LSW <sswon314@naver.com>
2022-11-12 16:17:30 +0900

    init-commit

---
+++ surmoonvey-discovery/src/test/java/com/example/surmoonveydiscovery/SurmoonveyDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surmoonveydiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurmoonveyDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
