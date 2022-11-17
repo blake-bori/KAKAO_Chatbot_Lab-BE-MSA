9e4971746c070c95fc1dd83423ac5d54ce1fe1be
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-11-17 12:15:22 +0900

    Delete surmoonvey-response directory

--- surmoonvey-response/src/main/java/com/example/goENC/repositories/ChoiceResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.goENC.repositories;
-
-import com.example.goENC.models.ChoiceResponse;
-import com.example.goENC.models.compositeKey.ChoiceResponseId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChoiceResponseRepository extends JpaRepository<ChoiceResponse, ChoiceResponseId> {
-}
--- surmoonvey-response/src/main/java/com/example/goENC/repositories/SubjectiveResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.goENC.repositories;
-
-import com.example.goENC.models.SubjectiveResponse;
-import com.example.goENC.models.compositeKey.SubjectiveResponseId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectiveResponseRepository extends JpaRepository<SubjectiveResponse, SubjectiveResponseId> {
-}
