439afb29fdeb5d147d52da4060a947549cea8e3c
LSW <sswon314@naver.com>
2022-11-22 15:22:45 +0900

    응답 서비스 제거

--- surmoonvey-response/src/main/java/com/example/surmoonveyresponse/repositories/ChoiceResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.surmoonveyresponse.repositories;
-
-import com.example.surmoonveyresponse.models.ChoiceResponse;
-import com.example.surmoonveyresponse.models.compositeKey.ChoiceResponseId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChoiceResponseRepository extends JpaRepository<ChoiceResponse, ChoiceResponseId> {
-}
--- surmoonvey-response/src/main/java/com/example/surmoonveyresponse/repositories/SubjectiveResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.surmoonveyresponse.repositories;
-
-import com.example.surmoonveyresponse.models.SubjectiveResponse;
-import com.example.surmoonveyresponse.models.compositeKey.SubjectiveResponseId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectiveResponseRepository extends JpaRepository<SubjectiveResponse, SubjectiveResponseId> {
-}
