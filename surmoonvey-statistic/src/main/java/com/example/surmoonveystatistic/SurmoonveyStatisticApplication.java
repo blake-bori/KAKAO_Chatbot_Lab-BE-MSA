3fc75cffb8642a331179cbec94b781a0405e6441
sswon314 <sswon314@naver.com>
2022-12-08 15:45:24 +0900

    배포 설정 추가

--- surmoonvey-statistic/src/main/java/com/example/surmoonveystatistic/SurmoonveyStatisticApplication.java
+++ surmoonvey-statistic/src/main/java/com/example/surmoonveystatistic/SurmoonveyStatisticApplication.java
@@ -2,10 +2,8 @@ package com.example.surmoonveystatistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SurmoonveyStatisticApplication {
 
 	public static void main(String[] args) {
